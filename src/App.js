07659472ea2fd5a587185abc95778383ba30f211
Kryak-dushi <fssashadrmatgat@gmail.com>
2021-07-02 15:00:47 +0400

    Main page created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './quiz.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div  className="App-button-container">
+          <button className="App-button start">Start</button>
+          <button className="App-button history">History</button>
+        </div>
       </header>
     </div>
   );
