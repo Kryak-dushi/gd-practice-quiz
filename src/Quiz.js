fe70546abf47dd7a1b392c32758dd9beb49588ba
Kryak-dushi <fssashadrmatgat@gmail.com>
2021-07-18 19:28:03 +0400

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,14 +4,16 @@ import Button from './Button';
 import Questions from './questions.json'
 
 function Quiz() {
-    let index = 0;
+    let quiz_index = 0;
+    let question_index = 0;
     return (
         <div className="mainpage question">
-            <p>{Questions.quizes[index].questions[index].question}</p>
-            <Button className="mainpage-button button-yellow question-button" text={Questions.quizes[index].questions[index].answers[0]} />
-            <Button className="mainpage-button button-yellow question-button" text={Questions.quizes[index].questions[index].answers[1]} />
-            <Button className="mainpage-button button-yellow question-button" text={Questions.quizes[index].questions[index].answers[2]} />
-            <Button className="mainpage-button button-yellow question-button" text={Questions.quizes[index].questions[index].answers[3]} />
+            <p>{Questions.quizes[quiz_index].questions[question_index].question}</p>
+            {
+                Questions.quizes[quiz_index].questions[question_index].answers.map((answer) => (
+                    <Button className="mainpage-button button-yellow question-button" text={answer} />
+                ))
+            }
         </div>
     );
 }
